a7dc86896ce9b555f70eb323521e69238e9f7980
songtao <songtao_st08@163.com>
2019-07-27 19:11:27 +0800

    init

---
+++ test/studyHttpServer.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+})
+
+server.listen(3000, function() {
+    console.log('server is listening at 3000 prot')
+})
